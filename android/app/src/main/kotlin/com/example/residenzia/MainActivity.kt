4d7a4102def49b987c9a9aef7a8f734c70af3dde
DivChadha <divyamchadha@gmail.com>
2024-03-17 00:45:37 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/residenzia/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.residenzia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart'
+// provides widgets for building the UI
+
+void main()
+{
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget
+// stateless widget is basically one which if you make 
+// you cannot change its properties, behaviour or appearances - it is immutable
+{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
