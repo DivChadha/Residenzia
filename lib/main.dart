b9ef81f97e283b2f1f93a96e76627037fe3ca496
DivChadha <divyamchadha@gmail.com>
2024-03-18 23:07:05 +0530

    try

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,13 @@ class MyApp extends StatelessWidget
 {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home : Scaffold(
+        body : Center(
+          child: Text('HomePage'),
+          )
+      )
+    );
   }
   
 }
